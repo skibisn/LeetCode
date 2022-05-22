af6f7bd530574de202643190b7e61d05c7abf870
JS <skibisn@gmail.com>
2022-05-22 15:07:40 +0900

    Time: 5 ms (86.47%), Space: 45.6 MB (50.72%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int num = getNumber(c);
+
+            if (i + 1 == s.length()) {
+                sum += num;
+                continue;
+            }
+
+            char next = s.charAt(i + 1);
+
+            if (num < getNumber(next)) {
+                sum -= num;
+            } else {
+                sum += num;
+            }
+        }
+
+        return sum;
+    }
+    
+    public int getNumber(char character) {
+        switch(character) {
+            case 'V': { return 5; }
+            case 'X': { return 10; }
+            case 'L': { return 50; }
+            case 'C': { return 100; }
+            case 'D': { return 500; }
+            case 'M': { return 1000; }
+            default: { return 1; }
+        }
+    }
+    
+    
+}
