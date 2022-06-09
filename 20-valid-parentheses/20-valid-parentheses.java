c951bfe920a1d4a5441e948fafe8e059735cfd0e
JS <skibisn@gmail.com>
2022-06-09 13:45:52 +0900

    Time: 4 ms (23.24%), Space: 41.6 MB (67.36%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            switch (c) {
+                case '(':
+                    stack.push(')');
+                    break;
+                case '{':
+                    stack.push('}');
+                    break;
+                case '[':
+                    stack.push(']');
+                    break;
+                default:
+                    if (stack.isEmpty() || c != stack.pop())
+                        return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
